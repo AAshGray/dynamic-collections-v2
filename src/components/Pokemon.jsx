e6687deee9651ccc81a37c23f7baa069711a753b
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-14 13:46:15 -0700

    added elements to pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,8 +1,16 @@
 export default function Pokemon(props) {
     return (
         <>
-            
-        
+            <h3>{props.name}</h3>
+            <img src={props.image} alt={props.name + " image"}></img>
+            <Type 
+                type= {props.type}
+            />
+            <p>HP: {props.hp}</p>
+            <p>Attack: {props.attack} Defense: {props.defense}</p>
+            <Abilities 
+                abilities={props.abilities}
+            />
         </>
     )
 } 
